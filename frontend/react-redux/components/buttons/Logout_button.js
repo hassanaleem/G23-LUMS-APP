919bfb79c296952e23a8b9dd83f7b7d588cbb971
Jawad Azhar Choudhary <76254826+jawadAzx@users.noreply.github.com>
2022-04-03 00:30:51 +0500

    minor fixes - TouchableOpacity

--- frontend/react-redux/components/buttons/Logout_button.js
+++ frontend/react-redux/components/buttons/Logout_button.js
@@ -1,45 +1,55 @@
-import React from 'react';
-import { View, Text, Image, Button, ScrollView, TextInput, StyleSheet, Alert, Pressable } from 'react-native';
-import {logout} from '../../actions/loginAction';
-import { useDispatch } from 'react-redux';
-import { useState } from 'react';
+import React from "react";
+import {
+  View,
+  Text,
+  Image,
+  Button,
+  ScrollView,
+  TextInput,
+  StyleSheet,
+  Alert,
+  Pressable,
+  TouchableOpacity,
+} from "react-native";
+import { logout } from "../../actions/loginAction";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
 
-export function Logout_button (props) {
-  const [loggedOut , setLoggedOut] = useState(false);
-  const dispatch = useDispatch()
-  const navigation = props.nav
+export function Logout_button(props) {
+  const [loggedOut, setLoggedOut] = useState(false);
+  const dispatch = useDispatch();
+  const navigation = props.nav;
   return (
-    <Pressable 
-      style={{alignItems: 'center',
-              justifyContent: 'center',
-              paddingVertical: 5,
-              paddingHorizontal: 10,
-              borderRadius: 30,
-              backgroundColor: '#79c4f2',
-              marginLeft: 250,
-              marginTop: 40,
-              marginRight:15,}} 
-
+    <TouchableOpacity
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+        paddingVertical: 5,
+        paddingHorizontal: 10,
+        borderRadius: 30,
+        backgroundColor: "#79c4f2",
+        marginLeft: 250,
+        marginTop: 40,
+        marginRight: 15,
+      }}
       onPress={() => {
         dispatch(logout());
-        Alert.alert('Logout Successful');
+        Alert.alert("Logout Successful");
         setLoggedOut(true);
-      }}>
-      
+      }}
+    >
       <Text style={styles.text}>Log out</Text>
-      {loggedOut ? navigation.navigate('Home'): null}
-    </Pressable>
+      {loggedOut ? navigation.navigate("Home") : null}
+    </TouchableOpacity>
   );
 }
 
 const styles = StyleSheet.create({
-
   text: {
     fontSize: 14,
     lineHeight: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     letterSpacing: 0.25,
-    color: 'white',
+    color: "white",
   },
-
-})
+});
