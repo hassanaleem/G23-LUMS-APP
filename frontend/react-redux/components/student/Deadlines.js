65f5f03e44f764a327274e472f781a1d6cf4cb68
Jawad Azhar Choudhary <76254826+jawadAzx@users.noreply.github.com>
2022-04-10 05:05:37 +0500

    Fixes

--- frontend/react-redux/components/student/Deadlines.js
+++ frontend/react-redux/components/student/Deadlines.js
@@ -1,68 +1,246 @@
 import React from "react";
-import { ImageBackground, StyleSheet, Text, View ,Button,Alert,Pressable,ScrollView,Dimensions} from "react-native";
+import {
+  ImageBackground,
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  Alert,
+  Pressable,
+  ScrollView,
+  Dimensions,
+} from "react-native";
 
 import { Logout_button } from "../buttons/Logout_button";
 import { Main_button } from "../buttons/Main_button";
 
-import { Logout_button } from '../buttons/Logout_button';
-import { Main_button } from '../buttons/Main_button';
 const { width, height } = Dimensions.get("screen");
 
-export const Deadlines = ({navigation}) => {
+export const Deadlines = ({ navigation }) => {
   var dict = [
-    {Course_code : "Cs 100", Title : "Intro to CS" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 100", Title : "Intro to CS" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 100", Title : "Intro to CS" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 100", Title : "Intro to CS" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 100", Title : "Intro to CS" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 100", Title : "Intro to CS" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 100", Title : "Intro to CS" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 100", Title : "Intro to CS" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 100", Title : "Intro to CS" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 100", Title : "Intro to CS" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 100", Title : "Intro to CS" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 100", Title : "Intro to CS" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 100", Title : "Intro to CS" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 100", Title : "Intro to CS" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 100", Title : "Intro to CS" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 100", Title : "Intro to CS" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 100", Title : "Intro to CS" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 100", Title : "Intro to CS" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 100", Title : "Intro to CS" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 100", Title : "Intro to CS" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 200", Title : "Intro to Programming" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 200", Title : "Intro to Programming" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 200", Title : "Intro to Programming" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 200", Title : "Intro to Programming" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 200", Title : "Intro to Programming" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 200", Title : "Intro to Programming" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 200", Title : "Intro to Programming" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 200", Title : "Intro to Programming" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 200", Title : "Intro to Programming" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 200", Title : "Intro to Programming" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 200", Title : "Intro to Programming" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 200", Title : "Intro to Programming" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 200", Title : "Intro to Programming" , Date : "10/12/2021", Time :"11 55 am" },
-    {Course_code : "Cs 200", Title : "Intro to Programming" , Date : "10/12/2021", Time :"11 55 am" },
+    {
+      Course_code: "Cs 100",
+      Title: "Intro to CS",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 100",
+      Title: "Intro to CS",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 100",
+      Title: "Intro to CS",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 100",
+      Title: "Intro to CS",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 100",
+      Title: "Intro to CS",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 100",
+      Title: "Intro to CS",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 100",
+      Title: "Intro to CS",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 100",
+      Title: "Intro to CS",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 100",
+      Title: "Intro to CS",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 100",
+      Title: "Intro to CS",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 100",
+      Title: "Intro to CS",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 100",
+      Title: "Intro to CS",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 100",
+      Title: "Intro to CS",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 100",
+      Title: "Intro to CS",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 100",
+      Title: "Intro to CS",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 100",
+      Title: "Intro to CS",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 100",
+      Title: "Intro to CS",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 100",
+      Title: "Intro to CS",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 100",
+      Title: "Intro to CS",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 100",
+      Title: "Intro to CS",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 200",
+      Title: "Intro to Programming",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 200",
+      Title: "Intro to Programming",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 200",
+      Title: "Intro to Programming",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 200",
+      Title: "Intro to Programming",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 200",
+      Title: "Intro to Programming",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 200",
+      Title: "Intro to Programming",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 200",
+      Title: "Intro to Programming",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 200",
+      Title: "Intro to Programming",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 200",
+      Title: "Intro to Programming",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 200",
+      Title: "Intro to Programming",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 200",
+      Title: "Intro to Programming",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 200",
+      Title: "Intro to Programming",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 200",
+      Title: "Intro to Programming",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+    {
+      Course_code: "Cs 200",
+      Title: "Intro to Programming",
+      Date: "10/12/2021",
+      Time: "11 55 am",
+    },
+  ];
+  return (
+    <ImageBackground
+      source={require("../assets/background.png")}
+      resizeMode="cover"
+      style={styles.backgroundImage}
+    >
+      <View style={styles.container}>
+        <Logout_button />
+        <Text style={styles.topheading}> Deadlines </Text>
 
- 
-  ]
-    return(
-      <ImageBackground source={require('../assets/background.png')} 
-      resizeMode="cover" 
-      style={styles.backgroundImage}>
-        <View style={styles.container}>
-          <Logout_button/>
-          <Text style = {styles.topheading}> Deadlines </Text>
-        
-        <ScrollView style = {styles.rectangle2}>
-        {dict.map((data, index) => (
-              <View key={index}>
-                <Text style={styles.textstyle}>
-                  {data.Course_code} : {data.Title} :  {data.Date} : {data.Time}
-                </Text>
-              </View>
-            ))}
+        <ScrollView style={styles.rectangle2}>
+          {dict.map((data, index) => (
+            <View key={index}>
+              <Text style={styles.textstyle}>
+                {data.Course_code} : {data.Title} : {data.Date} : {data.Time}
+              </Text>
+            </View>
+          ))}
         </ScrollView>
         <Main_button
           text="Go Back"
@@ -72,71 +250,67 @@ export const Deadlines = ({navigation}) => {
           marginleft={50}
           marginright={47}
         />
-        </View>
-      </ImageBackground>
-      
+      </View>
+    </ImageBackground>
+  );
+};
 
+const styles = StyleSheet.create({
+  backgroundImage: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    zIndex: -1,
+  },
+  container: {
+    flex: 1,
+    height: "100%",
+  },
 
-    )};
+  topheading: {
+    position: "absolute",
+    top: height / 27,
+    left: width / 30,
+    fontSize: 30,
+    lineHeight: 37.8,
+    fontWeight: "bold",
+  },
+  topheading2: {
+    position: "absolute",
+    top: height / 15,
+    left: width / 30,
+    fontSize: 30,
+    lineHeight: 37.8,
+    fontWeight: "bold",
+  },
+  logoutbuttonview: {
+    position: "absolute",
+    top: 50,
+    right: 10,
+  },
 
-
-    const styles = StyleSheet.create({
-      backgroundImage: {
-        position: "absolute",
-        top: 0,
-        left: 0,
-        right: 0,
-        bottom: 0,
-        zIndex: -1,
-      },
-      container: {
-        flex: 1,
-        height: "100%",
-      },
-    
-      topheading: {
-        position: "absolute",
-        top: height / 27,
-        left: width/30,
-        fontSize: 30,
-        lineHeight: 37.8,
-        fontWeight: "bold",
-      },
-      topheading2: {
-        position: "absolute",
-        top: height / 15,
-        left: width/30,
-        fontSize: 30,
-        lineHeight: 37.8,
-        fontWeight: "bold",
-      },
-      logoutbuttonview : {
-        position : "absolute",
-        top : 50,
-        right : 10
-      },
-    
-      rectangle2: {
-        position: "absolute",
-        width: width / 1.2,
-        height: height / 2,
-        top: height / 9.5,
-        left: width/14,
-        borderRadius: 7,
-        backgroundColor: "#EDEDED",
-      },
-      logoutbutton: {
-        backgroundColor : '#87CEFA',
-        padding : 5,
-        borderRadius : 180
-      },
-      event: {
-        color: "black",
-        position : "absolute",
-        left : 10,
-        top : 50,
-        fontSize: 15,
-        fontWeight: "bold",
-      }
-    });
-    
+  rectangle2: {
+    position: "absolute",
+    width: width / 1.2,
+    height: height / 2,
+    top: height / 9.5,
+    left: width / 14,
+    borderRadius: 7,
+    backgroundColor: "#EDEDED",
+  },
+  logoutbutton: {
+    backgroundColor: "#87CEFA",
+    padding: 5,
+    borderRadius: 180,
+  },
+  event: {
+    color: "black",
+    position: "absolute",
+    left: 10,
+    top: 50,
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+});
