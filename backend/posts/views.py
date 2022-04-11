2380e18fa8b6248ac22505a002735cdd7b89f294
Jawad Azhar Choudhary <76254826+jawadAzx@users.noreply.github.com>
2022-04-11 22:26:12 +0500

    posts actions

--- backend/posts/views.py
+++ backend/posts/views.py
@@ -9,24 +9,18 @@ from django.views.decorators.csrf import csrf_exempt
 
 def posts(request):
     if request.method == "GET": # this will handle fetching all posts and user related posts
-        db = database.connect_db()
         req = list(request.GET.items())
         id = req[0][1]
-        if id == "allposts": #working 
-            data = db.child("Data").child("Posts").get().val()
-            print(data)
-            data = list(data)
-            return render(request, 'posts.html', {'data': json.dumps(data)})
-        else:
-            data = db.child("Data").child("Posts").child(id).get().val()
-            return render(request, 'posts.html', {'data': json.dumps(data)})
+        if id == "all": #working 
+            db = database.connect_db()
+            data = db.child("Data").child("Posts").get()
+            return render(request, "posts.html", {"data": data.val()})
     elif request.method == "POST": # this will post a new post
         db = database.connect_db()
         data = request.body.decode("utf-8")
         data = json.loads(data) 	
         userID = data["user_id"]
         data.pop("user_id")
-        print(data, userID)
         ide = 0
         try:
             id = db.child("Data").child("Posts").child(userID).get().val()
@@ -35,13 +29,12 @@ def posts(request):
             pass
         db.child("Data").child("Posts").child(userID).child(ide).set(data)
         return render(request, 'posts.html')
-    elif request.method == "PUT": # this will update a post
+    elif request.method == "PUT": # this will handle like, unlike, comment
         db = database.connect_db()
         req = list(request.GET.items())
         action = req[0][1]
         userId = req[1][1]
         postId = req[2][1]
-        print(action, userId, postId)
         if action == "like":
             msg = request.body.decode("utf-8")
             msg = json.loads(msg)
@@ -82,11 +75,25 @@ def posts(request):
             data["comments"] = comment_str
             db.child("Data").child("Posts").child(userId).child(postId).set(data)
             return render(request, 'posts.html')
-        return render(request, 'posts.html')
+        elif action == "deletecomment":
+            msg = request.body.decode("utf-8")
+            msg = json.loads(msg)
+            comment = msg["comment"]
+            data = db.child("Data").child("Posts").child(userId).child(postId).get().val()
+            comment_str = data["comments"]
+            if comment in comment_str:
+                comment_str = comment_str.replace(comment+",", "")
+                comment_str = comment_str.replace(comment, "")
+                if comment_str[-1] == ",":
+                    comment_str = comment_str[:-1]
+            data["comments"] = comment_str
+            db.child("Data").child("Posts").child(userId).child(postId).set(data)
+            return render(request, 'posts.html')
 
     elif request.method == "DELETE": # this will delete a post
         db = database.connect_db()
         req = list(request.GET.items())
-        id = req[0][1]
-        db.child("Data").child("Posts").child(id).remove()
+        userid = req[0][1]
+        postid = req[1][1]
+        db.child("Data").child("Posts").child(userid).child(postid).remove()
         return render(request, 'posts.html')
