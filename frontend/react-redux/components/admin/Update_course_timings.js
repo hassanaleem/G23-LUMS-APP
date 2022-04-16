3b2e7f8e87f2d6d12ecc5d7684eab9a19da83ce7
Jawad Azhar Choudhary <76254826+jawadAzx@users.noreply.github.com>
2022-04-16 17:59:38 +0500

    minor fix

--- frontend/react-redux/components/admin/Update_course_timings.js
+++ frontend/react-redux/components/admin/Update_course_timings.js
@@ -46,6 +46,7 @@ export const Update_course_timings = ({ navigation }) => {
       Alert.alert("Please enter a search query");
     } else {
       dispatch(getCourse(searchQuery.toUpperCase()));
+      setSearchQuery("");
     }
   };
   let userState = useSelector((state) => state.courseReducer);
@@ -99,11 +100,11 @@ export const Update_course_timings = ({ navigation }) => {
       <ImageBackground
         source={require("../assets/background.png")}
         resizeMode="cover"
-        style={{ width: '100%', height: '100%' }}>
-          
-          <Logout_button nav = {navigation}/>
-          
-          <Text
+        style={{ width: "100%", height: "100%" }}
+      >
+        <Logout_button nav={navigation} />
+
+        <Text
           style={{
             position: "absolute",
             fontSize: 27,
@@ -135,6 +136,7 @@ export const Update_course_timings = ({ navigation }) => {
             setSearchQuery(text);
           }}
           onPress={makeSearch}
+          bar_value={searchQuery}
         />
 
         <Text style={styles.id_text1}>Course Timings</Text>
